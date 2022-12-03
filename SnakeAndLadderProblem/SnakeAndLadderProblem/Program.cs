db65e357a08e2eeeaeac3a6f3777676ccc0be774
ruchi06 <ruchi06@gmail.com>
2022-12-04 01:34:15 +0530

    Single Player At Position 0

---
+++ SnakeAndLadderProblem/SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************Welcome To Sanke and Ladder Game*************************\n");
+
+            SnakeLadderGame game = new SnakeLadderGame();
+            game.SnakeLadder();
+        }
+    }
+}
